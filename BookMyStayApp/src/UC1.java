6556f50adb251c812fef067c92f572e4a019ab29
naveen-nexus <nk4451@srmist.edu.in>
2026-03-24 14:48:58 +0530

    Use Case 1: Application Entry & Welcome Message

---
+++ BookMyStayApp/src/UC1.java
@@ -0,0 +1,10 @@
+public class UC1 {
+        public static void main(String[] args) {
+            System.out.println("Welcome to Hotel Booking System    ");
+            System.out.println("System is starting...");
+            System.out.println("Initialization complete...");
+            System.out.println("Application is ready to use!");
+        }
+    }
+
+
